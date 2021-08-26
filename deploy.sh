43609d745d0070e18f40184a2b5c1bee7f071baa
Willis Monroe <willismonroe@gmail.com>
2021-08-26 08:13:00 -0700

    deploy

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+# abort on errors
+set -e
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+git init
+# git checkout -b main
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:willismonroe/cams-mapping.git main:gh-pages
